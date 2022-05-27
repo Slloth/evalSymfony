e1ded9dd6db749e083a1fea653fea8b1fb2b4dd7
Slloth <fiquet.noah@gmail.com>
2022-05-27 21:34:13 +0200

    fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,31 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Produit;
+use App\Entity\User;
+use Faker\Factory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
 
 class AppFixtures extends Fixture
 {
+    private $faker;
+    public function __construct(private UserPasswordHasherInterface $hasher){
+        $this->faker = Factory::create('fr_FR');
+    }
+
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $user = new User();
+        $user   ->setEmail("admin@test.com")
+                ->setRoles(["ROLE_ADMIN"])
+        ;
+        $passwordHased = $this->hasher->hashPassword($user,$_ENV["ADMIN_PASSWORD"]);
+        $user->setPassword($passwordHased);
+
+        $manager->persist($user);
 
         $manager->flush();
     }
